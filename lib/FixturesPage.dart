e904efee986db5dbe866de008b29c74da0524d5e
QuangPeterNguyen <quangpeternguyen@gmail.com>
2025-07-12 23:42:37 +0700

    Improvements

--- lib/FixturesPage.dart
+++ lib/FixturesPage.dart
@@ -7,12 +7,12 @@ class FixturesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final List<Map<String, String>> fixtures = [
-      {'date': '17/07/2025', 'opponent': 'OBD FC'},
-      {'date': '24/07/2025', 'opponent': 'FAST FC'},
-      {'date': '31/07/2025', 'opponent': 'ANH EM FC'},
-      {'date': '07/08/2025', 'opponent': 'NHÀ FC'},
-      {'date': '14/08/2025', 'opponent': 'THIẾT BỊ VĂN PHÒNG FC'},
-      {'date': '21/08/2025', 'opponent': 'MIỀN NAM FC'},
+      {'date': '16/07/2025', 'opponent': 'FAST FC'},
+      {'date': '23/07/2025', 'opponent': 'OBD FC'},
+      {'date': '30/07/2025', 'opponent': 'ANH EM FC'},
+      {'date': '06/08/2025', 'opponent': 'NHÀ FC'},
+      {'date': '13/08/2025', 'opponent': 'THIẾT BỊ VĂN PHÒNG FC'},
+      {'date': '20/08/2025', 'opponent': 'MIỀN NAM FC'},
     ];
 
     final Color primaryColor = const Color(0xFF50BFE6); // Sky blue
