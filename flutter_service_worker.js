ab9bf739fc2279f945e008d6b9304c2f735de189
QuangPeterNguyen <quangpeternguyen@gmail.com>
2025-07-18 00:47:37 +0700

    Deploy ds2fc on Fri Jul 18 00:47:37 SEAST 2025

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -86,7 +86,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "23756ba66da0b78858bcc2159cb4bfdb
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "4e1c00567e5fd8927d26abb9fc4010e8",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "5c4bf05fbfc8feaeef9a81daa99bf528",
+"flutter_bootstrap.js": "117afa1b41f1132c2553befb8c9b9f51",
 "icons/Icon-192.png": "b858eec0d5dfa6aa3181e070f0afdded",
 "icons/Icon-512.png": "90395b7ead299b7700b1ed3760e1d29e",
 "icons/Icon-maskable-192.png": "b858eec0d5dfa6aa3181e070f0afdded",
