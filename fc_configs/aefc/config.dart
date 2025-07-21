cb8e29e56152bea0fa6a99efad53c9e05672279c
QuangPeterNguyen <quangpeternguyen@gmail.com>
2025-07-21 17:25:36 +0700

    Improvements

--- fc_configs/aefc/config.dart
+++ fc_configs/aefc/config.dart
@@ -13,55 +13,30 @@ final String teamLogo = 'assets/images/logo.png';
 //main.dart
 final List<String> bannerImages = [
 'assets/images/about_us_banner.png',
-'assets/images/banner1.jpg',
-'assets/images/banner2.jpg',
-'assets/images/banner3.jpg',
-'assets/images/banner4.jpg',
-'assets/images/banner5.jpg',
-'assets/images/banner6.jpg',
-'assets/images/banner7.jpg',
-'assets/images/banner8.jpg',
-'assets/images/banner9.jpg',
-'assets/images/banner10.jpg',
-'assets/images/banner11.jpg',
-'assets/images/banner12.jpg',
-'assets/images/banner13.jpg',
-'assets/images/banner14.jpg',
-'assets/images/banner15.jpg',
+'assets/images/about_us_banner.png',
+'assets/images/about_us_banner.png',
+'assets/images/about_us_banner.png',
 ];
 
 final List<Map<String, String>> recentScorers = [
 {'name': 'Hà Hoàng', 'image': 'assets/images/recent_scorers/haohoang.jpg'},
 {'name': 'Tran Nguyen Thailand', 'image': 'assets/images/recent_scorers/trannguyenthailand.jpg'},
+{'name': 'Tran Nguyen Thailand', 'image': 'assets/images/recent_scorers/trannguyenthailand.jpg'},
+{'name': 'Tran Nguyen Thailand', 'image': 'assets/images/recent_scorers/trannguyenthailand.jpg'},
 ];
 
 //Players.dart
 final players = [
-{'name': 'Hưng Đoàn', 'image': 'assets/images/players/hungdoan.jpg'},
-{'name': 'Dung Nguyen', 'image': 'assets/images/players/dungnguyen.jpg'},
-{'name': 'Hồ Công Hòa', 'image': 'assets/images/players/hoconghoa.jpg'},
-{'name': 'Hà Hoàng', 'image': 'assets/images/players/haohoang.jpg'},
-{'name': 'Trung Lê', 'image': 'assets/images/players/trungle.jpg'},
-{'name': 'Hoàng', 'image': 'assets/images/players/hoang.jpg'},
-{'name': 'Nguyen Thuy', 'image': 'assets/images/players/nguyenthuy.png'},
-{'name': 'Sinhgia2020', 'image': 'assets/images/players/sinhgia2020.jpg'},
-{'name': 'Duy Mập', 'image': 'assets/images/players/duymap.jpg'},
-
-{'name': 'Hậu Lê', 'image': 'assets/images/players/haule.jpg'},
-{'name': 'Thanh Tra', 'image': 'assets/images/players/thanhtra.jpg'},
-{'name': 'Tran Nguyen Thailand', 'image': 'assets/images/players/trannguyenthailand.jpg'},
-{'name': 'Thỏa Lê', 'image': 'assets/images/players/thoale.jpg'},
+{'name': 'Hưng Đoàn', 'image': 'assets/images/players/hungdoan.png'},
+{'name': 'Dung Nguyen', 'image': 'assets/images/players/dungnguyen.png'},
+{'name': 'Dung Nguyen', 'image': 'assets/images/players/dungnguyen.png'},
+{'name': 'Dung Nguyen', 'image': 'assets/images/players/dungnguyen.png'},
 ];
 
 //GaleryPage.dart
 final List<String> imagePaths = [
-'assets/images/banner1.jpg',
-'assets/images/banner2.jpg',
-'assets/images/banner3.jpg',
-'assets/images/banner4.jpg',
-'assets/images/banner5.jpg',
-'assets/images/banner6.jpg',
-'assets/images/banner7.jpg',
-'assets/images/banner8.jpg',
-'assets/images/banner9.jpg',
+'assets/images/dungnguyen.png',
+'assets/images/hungdoan.png',
+'assets/images/hungdoan.png',
+'assets/images/hungdoan.png',
 ];
--- fc_configs/aefc/theme.dart
+++ fc_configs/aefc/theme.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const primary = Color(0xFFE66482); //
+  static const primary = Color(0xFF961A1E);
   static const secondary = Color(0xFF222222); //
   static const background = Colors.white;
   static const text = Colors.black87;
