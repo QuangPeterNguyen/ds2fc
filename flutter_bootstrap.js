5a6f7f80f1ba37e2aa7b0ad6d2e1966032140e6f
QuangPeterNguyen <quangpeternguyen@gmail.com>
2025-07-24 12:50:59 +0700

    Deploy ds2fc on Thu Jul 24 12:50:59 SEAST 2025

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3334018600"
+    serviceWorkerVersion: "87995332"
   }
 });
