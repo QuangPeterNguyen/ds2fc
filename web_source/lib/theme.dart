68205e686504489b5682b48650a75dcc35b54a24
QuangPeterNguyen <quangpeternguyen@gmail.com>
2025-07-24 21:42:52 +0700

    Improvements

--- web_source/lib/theme.dart
+++ web_source/lib/theme.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const primary = Color(0xFF50BFE6);
-  static const secondary = Color(0xFF003366);
+  static const primary = Color(0xFF961A1E);
+  static const secondary = Color(0xFF222222); //
   static const background = Colors.white;
   static const text = Colors.black87;
   static const buttonTextIconColor = Colors.white;
